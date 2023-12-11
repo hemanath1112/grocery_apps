162c34f571ddfb6baa21b96b3ff065d7771822e8
Hemanath Chakkaravarthi <114560429+hemanath1112@users.noreply.github.com>
2023-12-11 23:52:14 +0530

    updated

--- src/Screens/UserPage.tsx
+++ src/Screens/UserPage.tsx
@@ -1,12 +1,25 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
+import {PoppinsRegular} from '../Components/Fonts';
 
 const UserPage = () => {
   return (
-    <View>
-      <Text>UserPage</Text>
+    <View style={styles.userContainer}>
+      <Text style={styles.textContainer}> No User</Text>
     </View>
   );
 };
 
 export default UserPage;
+
+const styles = StyleSheet.create({
+  userContainer: {
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textContainer: {
+    fontFamily: PoppinsRegular,
+    fontSize: 20,
+  },
+});
