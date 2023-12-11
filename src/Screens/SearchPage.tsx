5a41d17acc0292b053ffa5881eb1bc7fcbd2439f
Hemanath Chakkaravarthi <114560429+hemanath1112@users.noreply.github.com>
2023-12-11 23:43:42 +0530

    updated

--- src/Screens/SearchPage.tsx
+++ src/Screens/SearchPage.tsx
@@ -5,14 +5,31 @@ import {
   TextInput,
   StyleSheet,
   TouchableOpacity,
+  FlatList,
 } from 'react-native';
 import React, {useState} from 'react';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import {Black, Gray, Green, MindNightBule, White} from '../Components/Color';
-import {PoppinsLight} from '../Components/Fonts';
+import {Black, Gray, Green, White} from '../Components/Color';
+import {PoppinsLight, PoppinsRegular} from '../Components/Fonts';
+import Item from '../Components/Data';
+import {GroceryDetails} from '../Components/index';
 
 const SearchPage = () => {
-  const [search, setSearch] = useState();
+  const [search, setSearch] = useState('');
+  const [result, setResult] = useState([]);
+  console.log(result);
+
+  const searchHandler = () => {
+    const searchData = Item.filter(
+      item => item.name.toLowerCase() === search.toLowerCase(),
+    );
+    setResult(searchData);
+    console.log(searchData);
+  };
+
+  const renderItem = ({item}: any) => {
+    return <GroceryDetails item={item} />;
+  };
   return (
     <SafeAreaView>
       <View style={styles.serachContainer}>
@@ -21,15 +38,22 @@ const SearchPage = () => {
             value={search}
             placeholder="Search your Grocery Item"
             style={styles.searchItems}
-            // onChangeText={}
+            onChangeText={setSearch}
           />
         </View>
         <View style={styles.fontIcon}>
-          <TouchableOpacity>
+          <TouchableOpacity onPress={searchHandler}>
             <FontAwesome name="search" style={styles.iconText} />
           </TouchableOpacity>
         </View>
       </View>
+      {result.length === 0 ? (
+        <View style={styles.searchtem}>
+          <Text style={styles.searchText}> No Search Item</Text>
+        </View>
+      ) : (
+        <FlatList data={result} renderItem={renderItem} />
+      )}
     </SafeAreaView>
   );
 };
@@ -67,5 +91,14 @@ const styles = StyleSheet.create({
     borderBottomLeftRadius: 10,
     fontFamily: PoppinsLight,
     padding: 10,
+  },
+  searchtem: {
+    height: '90%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  searchText: {
+    fontFamily: PoppinsRegular,
+    fontSize: 20,
   },
 });
